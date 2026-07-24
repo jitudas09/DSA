0a5cfc368b22dd248b4b2a1812559f6defab00e9
jitu <jituniloy28@gmail.com>
2026-07-25 01:19:46 +0600

    Practice C++ class, encapsulation, setter and getter

---
+++ CLASS/class.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+class Teacher { 
+private : // Members of a class are private by default. 
+    double salary ; 
+
+public: 
+    // Public members can be accessed from outside the class.
+    string name ;          
+    string dept ; 
+    string subject ; 
+
+    void changedept (string newdept ){
+        dept = newdept ; 
+    }
+
+
+    // Setter and Getter are used to access private data safely.
+    // Setter function to assign salary
+    void setsalary( double s ){
+        salary = s ; 
+    }
+    
+    // Getter function to return salary
+    double getsalary(){
+        return salary ; 
+    }
+
+    
+} ;
+
+int main (){
+    Teacher t1 ; 
+    t1.name = "Jitu" ; 
+    t1.subject = "C++" ; 
+    t1.dept = "Computer Science " ; 
+    // t1.salary = 25000 ; 
+    t1.setsalary(25000) ;
+
+    cout << t1.name << endl ; 
+    cout << t1.getsalary() << endl ; 
+
+
+    return 0 ; 
+
+}
