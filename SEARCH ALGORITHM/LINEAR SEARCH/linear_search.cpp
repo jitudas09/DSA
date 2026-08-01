313142316d218006d6b0d9835f733ca43a114e2c
jitu <jituniloy28@gmail.com>
2026-08-01 21:07:23 +0600

    Linear Search using c++

---
+++ SEARCH ALGORITHM/LINEAR SEARCH/linear_search.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ; 
+
+int linear_search(int arr[] , int n, int key ) {
+
+    for (int i = 0 ; i<n ; i++){
+
+        if(arr[i]==key ) return i ;
+    }
+    return -1 ; 
+}
+
+int main (){
+
+    int arr[]={10,20,30,40,50} ;
+    int n = sizeof(arr) / sizeof(arr[0]) ;
+
+    int key = 30 ; 
+    int result = linear_search(arr,n,key) ; 
+
+    if(result !=-1 )  cout << key << " found at index " << result << endl ; 
+    else cout << key << " not found." << endl ;
+
+    return 0 ;
+}
