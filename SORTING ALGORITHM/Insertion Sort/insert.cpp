43ee8cf3f5ee7cd3e6f10c21501f26b6f50b44f8
jitu <jituniloy28@gmail.com>
2026-08-18 21:37:09 +0600

    Insertion Sort using C++

--- SORTING ALGORITHM/Insertion Sort/insert.cpp
+++ SORTING ALGORITHM/Insertion Sort/insert.cpp
@@ -2,6 +2,20 @@
 using namespace std ; 
 
 int main (){
-    cout << "Jitu das" << endl ; 
-    return 0 ; 
+    int arr[5]={43,31,26,23,12} ; 
+    int n= 5 ; 
+    for ( int i = 1 ; i<n ; i++){
+        int key = arr[i] ; 
+        int j= i-1 ;
+        while(j>=0 && arr[j]> key){
+            arr[j+1]= arr[j] ;
+            j-=1 ;
+            arr[j+1]= key ;
+        }
+    }
+    for(int i = 0 ;i<n ; i++){
+        cout << arr[i] << ' ' ;
+    }
+
+    return 0 ;
 }
